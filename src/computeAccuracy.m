b59390d5081ff7e417200cc2e518654c55508614
Siddarth Malreddy <malreddysid@gmail.com>
2016-11-24 15:10:50 -0500

    Added functionality to check accuracy

--- src/computeAccuracy.m
+++ src/computeAccuracy.m
@@ -2,10 +2,12 @@
 GT = csvread('../data/BasketBall/groundtruth_rect.txt');
 
 ours = csvread('rects.txt');
+ours_conv = csvread('rects_conv.txt');
 
 num_frames = size(GT, 1);
 
 assert(size(GT, 1) == size(ours, 1));
+assert(size(GT, 1) == size(ours_conv, 1));
 
 area_overlap = rectint(GT, ours);
 area_overlap = diag(area_overlap);
@@ -20,7 +22,23 @@ for i = 1:num_t
     num_correct(i) = sum(area_overlap > GT_area.*overlap_threshold(i))/num_frames;
 end
 
-plot(overlap_threshold, num_correct, 'r', 'LineWidth', 2);
+plot(overlap_threshold, num_correct, 'r', 'LineWidth', 1);
 title('Accuracy vs OverlapThreshold');
 xlabel('OverlapThreshold');
 ylabel('Accuracy');
+hold on;
+
+area_overlap = rectint(GT, ours_conv);
+area_overlap = diag(area_overlap);
+
+GT_area = GT(:,3).*GT(:,4);
+
+overlap_threshold = 0:0.01:1;
+num_t = numel(overlap_threshold);
+num_correct = zeros(1, num_t);
+
+for i = 1:num_t
+    num_correct(i) = sum(area_overlap > GT_area.*overlap_threshold(i))/num_frames;
+end
+
+plot(overlap_threshold, num_correct, 'b', 'LineWidth', 1);
